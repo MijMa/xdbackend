19107910239ccdc020823bdd2e1d29d6820090af
mijma <mio.maxx@hotmail.com>
2025-10-23 23:35:44 +0300

    First broadcast not sent bug

--- src/routes/util/broadCastParticipantCount.ts
+++ src/routes/util/broadCastParticipantCount.ts
@@ -1,12 +1,19 @@
+import { IncomingMessage, ServerResponse } from "http";
 import { getParticipantCount } from "./getParticipantCount.js";
 
-export function broadcastParticipantCount(formId: string, clients: Set<any>) {
+type FastifyReply = {
+  reply: ServerResponse<IncomingMessage>
+}
+
+export async function broadcastParticipantCount (formId: string, clients: Set<FastifyReply["reply"]>) {
   console.log("BROADCAST TRIGGERED");
-  console.log("BROADCAST TRIGGERED, CLIENTS", clients);
-  const count = getParticipantCount(formId); // fetch latest count
+  console.log("BROADCAST TRIGGERED, CLIENTS", clients.size);
+  const count = await getParticipantCount(formId); // fetch latest count
+  console.log("Count received from call: ", count);
 
   for (const client of clients) {
-    console.log("WRITE TRIGGERED");
-    client.write(`data: ${JSON.stringify({ count })}\n\n`);
+    console.log("WRITE TRIGGERED ", `data: ${JSON.stringify({ count })}\n\n`);
+    client.write(`data: ${count}\n\n`);
+    client.write(`data: ${count}\n\n`);
   }
 }
